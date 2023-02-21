5c4f596606281c5227450285b3022ac6c5af0102
Sejo Basic <sejoob@gmail.com>
2023-02-20 18:12:46 -0700

    Hero Styling layout complete

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -1,10 +1,26 @@
+import Link from 'next/link'
 import React from 'react'
 
 function HeroBanner() {
   return (
     <div className='hero-banner-container'>
       <div>
-        <p className='hero-sofa'></p>
+        <p className='hero-sofa'>SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img
+          className='hero-banner-image'
+          src=''
+          alt='Logan Upholstered Loveseat'
+        />
+        <div>
+          <Link href='/product/ID'>
+            <button type='button'>TEXT</button>
+          </Link>
+          <div className='desc'>
+            <h5>Description</h5>
+            <p>DESC HERE</p>
+          </div>
+        </div>
       </div>
     </div>
   )
